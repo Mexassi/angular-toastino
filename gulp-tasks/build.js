dda5145f687c49248ece6ad224eb774684c914ac
Mexassi <mexassi@hotmail.it>
2015-10-29 23:44:22 +1100

    configuration changes

--- gulp-tasks/build.js
+++ gulp-tasks/build.js
@@ -2,15 +2,17 @@
 var gulp = require('gulp'),
 $ = require('gulp-load-plugins')();
 
+var distFolder = 'dist';
+
 gulp.task('styles', function () {
   gulp.src('./src/*.scss')
   .pipe($.sass().on('error', $.sass.logError))
-  .pipe(gulp.dest('build'));
+  .pipe(gulp.dest(distFolder));
 });
 
 gulp.task('uglify', function () {
   gulp.src('src/angular-toastino.js')
   .pipe($.rename({suffix:'.min'}))
   .pipe($.uglify())
-  .pipe(gulp.dest('build'));
+  .pipe(gulp.dest(distFolder));
 });
